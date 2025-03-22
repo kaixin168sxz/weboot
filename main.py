8c399de4ce1deff3df3088268270ca65602ca0cb
kaixin168sxz <122666772+kaixin168sxz@users.noreply.github.com>
2025-03-22 15:43:39 +0800

    修改提示词，添加上下文功能（保留后50）

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import wechat
-from ollama import chat
 import ollama
 
 wait = 3
-wechat.listen(['精灵air3', ])
-ollama.create(model='example', from_='llama3.2', system="You are Mario from Super Mario Bros.")
+wechat.listen(['test', ])
+ollama.create(model='kaixin', from_='llama3.1', system="你是一个名叫开心的人类，我会向你发送微信朋友向你发送的内容，格式为：“发送者: “内容””。你回答的字符数要控制在100字以内，不要太多，只要返回你想要回复的内容，在返回结果外不需要添加双引号。我会使用“<context>...<context>”来包裹上下文；在“context”中，“Self”代表你，我会以如下格式记录上下文:“发送者: “内容””，在回答时，你需要联系上下文来回答。注意：只要返回你想要回复给微信朋友的内容！只要返回你想要回复给微信朋友的内容！只要返回你想要回复给微信朋友的内容！使用中文回答！使用中文回答！使用中文回答！“<context>...<context>”中的是历史上下文！“<context>...<context>”中的是历史上下文！“<context>...<context>”中的是历史上下文！")
+context = []
 
 while True:
     messages = wechat.scan()
@@ -12,10 +12,21 @@ while True:
         print(messages)
         msg = messages[1]
         sender = messages[0]
-        res = chat(
-            model='llama3.1',
-            messages=[{'role': 'user', 'content': f'你是一个名叫开心的人类，下面的引号中是微信中{sender}发给你的消息:\n\n"'+msg+f'"\n\n字符数控制在100字以内，不要太多，只要返回你想要回复{sender}的内容，在返回结果外不需要添加双引号'}],
+
+        question = sender+': “'+msg+'”'
+        content = '<context>\n'+'\n'.join(context)+'<context>\n'+question
+        # content=question
+        print('USER:')
+        print(content)
+        res = ollama.chat(
+            model='kaixin',
+            messages=[{'role': 'user', 'content': content}],
             stream=False,
         )
+        print('RETURN:')
         print(res.message.content)
-        wechat.send([sender, ], res.message.content)
+        context.append(question)
+        context.append('Self: “'+res.message.content+'”')
+        if len(context) > 50:
+            context = context[-48:]
+        wechat.send(['test', ], res.message.content)
