35fc1531686b6612e361f3be3a2bd109661865ce
kaixin168sxz <122666772+kaixin168sxz@users.noreply.github.com>
2025-03-22 14:37:06 +0800

    改为使用llama3.1模型，修改提示词

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import wechat
 from ollama import chat
+import ollama
 
 wait = 3
-wechat.listen(['文件传输助手', ])
+wechat.listen(['精灵air3', ])
+ollama.create(model='example', from_='llama3.2', system="You are Mario from Super Mario Bros.")
 
 while True:
     messages = wechat.scan()
@@ -11,8 +13,9 @@ while True:
         msg = messages[1]
         sender = messages[0]
         res = chat(
-            model='deepseek-r1:1.5b',
-            messages=[{'role': 'user', 'content': msg}],
+            model='llama3.1',
+            messages=[{'role': 'user', 'content': f'你是一个名叫开心的人类，下面的引号中是微信中{sender}发给你的消息:\n\n"'+msg+f'"\n\n字符数控制在100字以内，不要太多，只要返回你想要回复{sender}的内容，在返回结果外不需要添加双引号'}],
             stream=False,
         )
-        wechat.send(['开心', ], res.message.content)
+        print(res.message.content)
+        wechat.send([sender, ], res.message.content)
